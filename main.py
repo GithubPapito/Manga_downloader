fa032ad548933e6f1a54671bb881d8e1cc74e63d
Manga downloader <omelyanenko.01@mail.ru>
2024-12-23 13:30:20 +0300

    v0.3b

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import re
-import urllib.request
 from bs4 import BeautifulSoup
 import requests
-import sys
 import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import img2pdf
+from tqdm import tqdm
+import httplib2
+import random
+import logging
 
 class MangaDown:
     def __init__(self, url):
@@ -15,6 +19,7 @@ class MangaDown:
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
         self.links = []
         self.manga_name = ''
+        self.my_cwd = os.getcwd()
 
         self.get_manga_data()
         self.get_chapter_links()
@@ -25,7 +30,7 @@ class MangaDown:
     def check_status(self, status_code):
         if status_code != 200:
             print(f'TERMINATED! Server return code {status_code}!')
-            sys.exit(0)
+            exit(0)
 
     def get_manga_data(self):
         response = requests.get(self.url, headers=self.headers)
@@ -46,11 +51,12 @@ class MangaDown:
             for lnk in div.find_all('a', class_="chapter-link"):
                 self.links.append(lnk['href'])
 
+        self.links.reverse()
+
     def create_path(self):
-        my_cwd = os.getcwd()
         regex = re.compile('[\\\/\:\*\?\""\<\>\|]')
         self.manga_name = regex.sub(' ', self.manga_name)
-        path = os.path.join(my_cwd, self.manga_name)
+        path = os.path.join(self.my_cwd, self.manga_name)
 
         if not os.path.exists(path):
             os.mkdir(path)
@@ -60,96 +66,90 @@ class MangaDown:
             vol = a[2]
             ch = a[3]
             new_dir = str(vol)
-            path = os.path.join(my_cwd + '\\' + self.manga_name, new_dir)
+            path = os.path.join(self.my_cwd + '\\' + self.manga_name, new_dir)
 
             if not os.path.exists(path):
                 os.mkdir(path)
 
             new_dir = str(ch)
-            path = os.path.join(my_cwd + '\\' + self.manga_name + '\\' + str(vol), new_dir)
+            path = os.path.join(self.my_cwd + '\\' + self.manga_name + '\\' + str(vol), new_dir)
             if not os.path.exists(path):
                 os.mkdir(path)
 
     def download(self):
-        my_cwd = os.getcwd()
         url = self.url[:self.url.rfind('/')]
+        logging.getLogger('selenium').setLevel(logging.WARNING)
         driver = webdriver.Edge()
+        first = True
 
         for i in self.links:
             a = i.split('/')
             vol = a[2]
             ch = a[3]
-            path = os.path.join(my_cwd + '\\' + self.manga_name + '\\' + vol + '\\' + ch)
+            path = os.path.join(self.my_cwd + '\\' + self.manga_name + '\\' + vol + '\\' + ch)
             driver.get(url + i)
-            btn = driver.find_elements(By.CLASS_NAME, 'btn-lg')
-            time.sleep(1.3)
 
-            try:
-                btn[0].click()
-            except:
-                pass
+            if first:
+                try:
+                    element = WebDriverWait(driver, 5).until(
+                        EC.visibility_of_element_located((By.CLASS_NAME, 'btn-lg'))
+                    )
+                    element.click()
+                    first = False
+                except:
+                    first = False
 
-            no = driver.find_element(By.CLASS_NAME, 'pages-count')
+            time.sleep(0.5)
 
-            for y in range(int(no.text)):
-                errCount = 0
+            no = WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, 'pages-count'))
+            )
 
-                while True:
-                    if errCount > 100:
-                        print('Error while downloading file ' + i)
-                        break
-                    try:
-                        img = driver.find_element(By.CLASS_NAME, 'manga-img')
-                    except:
-                        errCount += 1
-                        print('Ошибка "1" ' + str(errCount))
-                        time.sleep(0.2)
-                        continue
-                    break
+            for y in tqdm (range(int(no.text)), desc=i):
+
+                img = WebDriverWait(driver, 5).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, 'manga-img'))
+                )
 
                 src = img.get_attribute('src')
                 fileType = src.split(".")[-1]
 
-                errCount = 0
-
-                while True:
-                    if errCount > 100:
-                        print('Error while downloading file ' + i)
-                        break
-                    try:
-                        urllib.request.urlretrieve(src, os.path.join(path, str(y) + "." + fileType[0:3]))
-                    except:
-                        errCount += 1
-                        print('Ошибка "2" ' + str(errCount))
-                        time.sleep(0.2)
-                        continue
-                    break
+                h = httplib2.Http('.cache')
+                response, content = h.request(src)
+                with open(os.path.join(path, str(y) + "." + fileType[0:3]), 'wb') as f:
+                    f.write(content)
 
                 errCount = 0
 
                 while True:
-                    if errCount > 100:
-                        print('Error while downloading file ' + i)
+                        if errCount > 100:
+                            driver.close()
+                            print('not good')
+                            exit(0)
+                        try:
+                            element = WebDriverWait(driver, 5).until(
+                                EC.visibility_of_element_located((By.CLASS_NAME, 'nextButton'))
+                            )
+                            element.click()
+                        except:
+                            errCount += 1
+                            time.sleep(0.2)
+                            continue
                         break
-                    try:
-                        btn = driver.find_element(By.CLASS_NAME, 'nextButton')
-                        btn.click()
-                    except:
-                        errCount += 1
-                        print('Ошибка "3" ' + str(errCount))
-                        time.sleep(0.2)
-                        continue
-                    break
 
-                time.sleep(0.7)
+                time.sleep(random.uniform(0.2, 0.4))
+
+        driver.close()
+        print('Скачивание завершен')
 
     def conwert_to_pdf(self):
-        my_cwd = os.getcwd()
-        path = os.path.join(my_cwd + '\\' + self.manga_name)
+        print('Создание PDF')
+
+        path = os.path.join(self.my_cwd + '\\' + self.manga_name)
         dir = os.listdir(path)
 
         for n in dir:
-            path = os.path.join(my_cwd + '\\' + self.manga_name + '\\' + n)
+            path = os.path.join(self.my_cwd + '\\' + self.manga_name + '\\' + n)
             vol = os.listdir(path)
             image_files = []
             ch = []
@@ -165,14 +165,12 @@ class MangaDown:
                     image_files.append(path + '\\' + str(v) + '\\' + i)
 
             pdf_data = img2pdf.convert(image_files)
-            with open(my_cwd + '\\' + self.manga_name + '\\' + n + ".pdf", "wb") as file:
+            with open(self.my_cwd + '\\' + self.manga_name + '\\' + n + ".pdf", "wb") as file:
                 file.write(pdf_data)
 
+        print('Создание PDF завершено')
 
 if __name__ == "__main__":
-    print("Адрес манги")
+    print("Введите адрес манги")
     url = input()
     MangaDown(url)
-
-
-
