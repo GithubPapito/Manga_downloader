fe1cb6b21798d8ce26657afb0c0089ba16489c2e
Manga downloader <omelyanenko.01@mail.ru>
2025-08-04 14:01:49 +0300

    v0.6.7

--- utils.py
+++ utils.py
@@ -75,4 +75,4 @@ def check_status(status_code):
 
 def sanitize_filename(name):
     """Очищает имя от недопустимых символов."""
-    return re.sub(r'[\\/:*?"<>|~]', ' ', name)
+    return re.sub(r'[\\/:*?"<>|~]', '_', name)
